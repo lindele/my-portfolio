75cb76ff087d59bcdf25f8f1fd3477238eb51f43
lindele <lindele@wwu.edu>
2018-12-21 19:40:40 -0800

    Use SNS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,23 +1,32 @@
 import boto3
 from botocore.client import Config
+import StringIO
 import zipfile
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3')
-
-    portfolio_bucket = s3.Bucket('portfolio.ethanlindell.com')
-    build_bucket = s3.Bucket('portfoliobuild.ethanlindell.com')
-
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    print "Job done!"
-
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:400552772338:deployPortfolioTopic')
+   
+    try:
+        s3 = boto3.resource('s3')
+        
+        portfolio_bucket = s3.Bucket('portfolio.ethanlindell.com')
+        build_bucket = s3.Bucket('portfoliobuild.ethanlindell.com')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')   
+        print "Job done!"
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully")
+        raise 
+        
     return 'Hello from Lambda!'
