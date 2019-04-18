86daaf1e5ed4a87f0b8d64f238606a55d15ea0df
lindele <lindele@wwu.edu>
2019-04-18 13:25:07 -0700

    Added portfolio/dynamic project section, updated resume, added section titles.

--- js/main.js
+++ js/main.js
@@ -4,34 +4,33 @@ import ExampleWork from './example-work.js';
 
 const myWork = [
   {
-    'title': "First time here?",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'title': "ICT Development",
+    'href': "images/BeaconPoster.pdf",
+    'desc': "There are 17 Sustainable Development Goals (SDGs) adopted by all United Nations members. These goals seek to, '...improve health and education, reduce inequality, and spur economic growth - all while tackling climate change and working to preserve our oceans and forests.' As a group of four, we decided to focus on SDG number 3, 'Good Health and Well-Being'. Specifically we wanted to use Information and Communication Technologies (ICT) to help those who suffer from anxiety and depression. Over the course of 10 weeks we researched, designed and developed a working prototype of our solution. We implemented the prototype using react-native and Expo.",
     'image': {
-      'desc': "example screenshot of a project involving code",
+      'desc': "beacon logo",
+      'src': "images/BeaconWelcome.png",
+      'comment': ""
+    }
+  },
+ {
+    'title': "'make' Program",
+    'href': "https://linux.die.net/man/1/make",
+    'desc':"Developed a make program over the course of 10 weeks. The functionality included targets, dependencies and rules. When invoked on a target the program follows dependencies and executes rules to prepare files. Supports I/O redirection, handles append, truncate and redirecting input (>>, >, <). Omits line following a pound sign (#) for commenting as well as environment variable assignment. Linked is the make man page, send me an email for accessing the program. Due to the academic honesty code of conduct I am unable to make it openly available online.",
+     'image': {
+      'desc': "",
       'src': "images/umake.png",
       'comment': ""
     }
   },
   {
-    'title': "Portfolio Boilerplate",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'title': "This Portfolio!",
+    'href': "https://github.com/lindele/my-portfolio",
+    'desc': "Created this online portfolio using A Cloud Guru's online course, 'Create a Serverless Portfolio with AWS and ReactJS'. After taking this course I took a University course where we developed an application using React-Native (see Beacon) and have continued to update my portfolio. In the course I learned to automate deployment using CodeBuild, CloudFront, S3 and Lamda.",
     'image': {
-      'desc': "A serverless Portfolio",
-      'src': "images/example2.png",
+      'desc': "",
+      'src': "images/",
       'comment': ""
-    }
-  },
-  {
-    'title': "Work Example",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    'image': {
-      'desc': "example screenshot of a project involving cats",
-      'src': "images/example3.png",
-      'comment': `"Bengal cat" by roberto shabs is licensed under CC BY 2.0
-          https://www.flickr.com/photos/37287295@N00/2540855181`
     }
   }
 ];
